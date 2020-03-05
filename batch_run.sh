d6d26935bdeca1a7259cf3933c2fdb90f1266850
alex <homaralex@gmail.com>
2020-03-05 21:51:03 +0100

    fix batch run scripts

--- batch_run.sh
+++ batch_run.sh
@@ -2,7 +2,7 @@ for i in {1..5}; do
   for penalty in "dec" "enc" "enc_dec"; do
     for z_dim in 32 256; do
       for lmbd in ".001" ".01" ".1" "1"; do
-        python run.py --experiment celebA_random_col_dec --z_logvar_regularisation col_L1_$penalty --lambda_logvar_regularisation $lmbd --z_dim $z_dim --experiment_path experiments/celebA_col_${penalty}/lmbd_${lmbd}_run_${i} --dataset celebA_mini
+        python run.py --experiment celebA_random_col_dec --z_logvar_regularisation col_L1_$penalty --lambda_logvar_regularisation $lmbd --z_dim $z_dim --experiment_path experiments/celebA_${z_dim}/col_${penalty}/lmbd_${lmbd}_run_${i}
       done
     done
   done
