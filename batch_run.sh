04a73f9fb32c69051b37602949761a1a0f001f9c
alex <homaralex@gmail.com>
2020-03-05 19:03:25 +0100

    update batch run scripts

--- batch_run.sh
+++ batch_run.sh
@@ -1,8 +1,8 @@
-for penalty in "dec" "enc" "enc_dec"; do
-  for i in {1..5}; do
+for i in {1..5}; do
+  for penalty in "dec" "enc" "enc_dec"; do
     for z_dim in 32 256; do
       for lmbd in ".001" ".01" ".1" "1"; do
-        python run.py --experiment celebA_random_col_dec --z_logvar_regularisation col_L1_$penalty --lambda_logvar_regularisation $lmbd --z_dim $z_dim --experiment_path experiments/celebA_col_${penalty}/lmbd_${lmbd}_run_${i}
+        python run.py --experiment celebA_random_col_dec --z_logvar_regularisation col_L1_$penalty --lambda_logvar_regularisation $lmbd --z_dim $z_dim --experiment_path experiments/celebA_col_${penalty}/lmbd_${lmbd}_run_${i} --dataset celebA_mini
       done
     done
   done
---
+++ batch_run_baseline.sh
@@ -0,0 +1,7 @@
+for i in {1..5}; do
+  for z_dim in 32 256; do
+    for lmbd in "0" ".001" ".01" ".1" "1"; do
+      python run.py --experiment celebA_random --z_logvar_regularisation L1 --lambda_logvar_regularisation $lmbd --z_dim $z_dim --experiment_path experiments/celebA_random/lmbd_${lmbd}_run_${i}
+    done
+  done
+done
