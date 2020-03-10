57051c568132461f4e3b9207b352bf7ff68652aa
alexander.rakowski <alexander.rakowski@hpi.de>
2020-03-10 13:42:55 +0100

    create scripts directory; download_results.sh script

---
+++ scripts/download_results.sh
@@ -0,0 +1,19 @@
+# this script is used to download results from experiments where test reconstruction error is measured
+# at the end of training (so far only CelebA)
+
+# file name with per-experiment configuration dictionary
+OPTS_FILENAME='opts.txt'
+# directory to download the files to on the local machine
+DOWNLOAD_PATH='results_download'
+mkdir $DOWNLOAD_PATH
+
+TEST_ERROR_PATHS=$(ssh $1 find "Wasserstein-Auto-Encoders/experiments/" -type f -name 'test_error.txt')
+for TEST_ERROR_PATH in $TEST_ERROR_PATHS; do
+  # create experiment directory tree on local machine
+  mkdir -p ${DOWNLOAD_PATH}/$(dirname $TEST_ERROR_PATH)
+  # download file with test_error
+  rsync ${1}:${TEST_ERROR_PATH} ${DOWNLOAD_PATH}/${TEST_ERROR_PATH}
+  # create path to the opts file and download it too
+  OPTS_PATH=$(dirname $TEST_ERROR_PATH)/${OPTS_FILENAME}
+  rsync ${1}:${OPTS_PATH} ${DOWNLOAD_PATH}/${OPTS_PATH}
+done
