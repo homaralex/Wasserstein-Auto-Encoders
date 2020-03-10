91db11dbfb7a1f658ee61e8b3f64a0933bce6c0e
alexander.rakowski <alexander.rakowski@hpi.de>
2020-03-10 16:29:23 +0100

    update lambda range for logvar penalty script

--- scripts/batch_run_baseline.sh
+++ scripts/batch_run_baseline.sh
@@ -1,6 +1,6 @@
 for i in {1..5}; do
   for z_dim in 32 256; do
-    for lmbd in "0" ".001" ".01" ".1" "1"; do
+    for lmbd in "0" ".001" ".01" ".1" "1" "10"; do
       python run.py --experiment celebA_random --z_logvar_regularisation L1 --lambda_logvar_regularisation $lmbd --z_dim $z_dim --experiment_path experiments/celebA_${z_dim}/random/lmbd_${lmbd}_run_${i}
     done
   done
