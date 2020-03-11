e08f53a12409135bb146fed2a095740a8bf6438e
alexander.rakowski <alexander.rakowski@hpi.de>
2020-03-11 15:48:30 +0100

    batch_fid.sh - skip directories where FID has already been computed

--- scripts/batch_fid.sh
+++ scripts/batch_fid.sh
@@ -1,4 +1,10 @@
 CHECKPOINT_PATHS=$(find $1 -type f -name 'checkpoint')
 for CHECKPOINT_PATH in $CHECKPOINT_PATHS; do
-  python fid.py --experiment_path $(dirname $(dirname $CHECKPOINT_PATH))
+  EXPERIMENT_PATH=$(dirname $(dirname $CHECKPOINT_PATH))
+  FID_PATH=${EXPERIMENT_PATH}/test_fid.txt
+  if [ ! -f ${FID_PATH} ]; then
+    echo "${FID_PATH} exists - skipping directory"
+  else
+    python fid.py --experiment_path $EXPERIMENT_PATH
+  fi
 done
