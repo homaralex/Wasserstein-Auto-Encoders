fix scaling by load_results.py

scripts/load_results.py
@@ -22,6 +22,10 @@ for test_error_file in DOWNLOAD_PATH.glob(f'**/{TEST_ERROR_FILENAME}'):
     opts_dict = pickle.load(opts_file.open('rb'))
     opts_dict['test_rec_error'] = test_error
 
+    # omit zero values due to log-scaling of the x-axis in the plots
+    if opts_dict['lambda_logvar_regularisation'] == 0:
+        opts_dict['lambda_logvar_regularisation'] = 1e-4
+
     rows.append(opts_dict)
 
 df = pd.DataFrame(rows)
@@ -35,7 +39,7 @@ ncols = df.z_logvar_regularisation.nunique()
 nrows = int(np.ceil(grouped.ngroups / ncols))
 
 for use_orig_scale in [True, False]:
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(12, 8), sharey='row')
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(16, 8), sharey='row')
 
     for (key, ax) in zip(grouped.groups.keys(), axes.flatten()):
         subplot = grouped.get_group(key).plot.scatter(
@@ -45,7 +49,7 @@ for use_orig_scale in [True, False]:
             label=key,
             ax=ax,
         )
-        ax.set_xlim((.0005, 1.5))
+        ax.set_xlim((5e-5, 12.5))
         if use_orig_scale:
             ax.set_ylim((6350, 6550) if key[0] == 32 else (6250, 6450))
 