e5d01b28998bc278ea60e04ac46f7e291af5e58d
alexander.rakowski <alexander.rakowski@hpi.de>
2020-03-11 15:34:30 +0100

    load_results.py - separate plots only for col_dec

--- scripts/load_results.py
+++ scripts/load_results.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 DOWNLOAD_PATH = Path('results_download')
 OPTS_FILENAME = 'opts.pickle'
+FID_FILENAME = 'test_fid.txt'
 TEST_ERROR_FILENAME = 'test_error.txt'
 PLOTS_DIR = Path('plots')
 PLOTS_DIR.mkdir(exist_ok=True)
@@ -22,6 +23,10 @@ for test_error_file in DOWNLOAD_PATH.glob(f'**/{TEST_ERROR_FILENAME}'):
     opts_dict = pickle.load(opts_file.open('rb'))
     opts_dict['test_rec_error'] = test_error
 
+    fid_score_file = test_error_file.parent / FID_FILENAME
+    if fid_score_file.exists():
+        opts_dict['test_fid_score'] = float(fid_score_file.read_text())
+
     # omit zero values due to log-scaling of the x-axis in the plots
     if opts_dict['lambda_logvar_regularisation'] == 0:
         opts_dict['lambda_logvar_regularisation'] = 1e-4
@@ -35,23 +40,30 @@ grouped = df.groupby([
 ])
 print(grouped.test_rec_error.mean())
 
-ncols = df.z_logvar_regularisation.nunique()
-nrows = int(np.ceil(grouped.ngroups / ncols))
+for use_orig_scale in (True, False):
+    for all_methods in (True, False):
+        grouped = (df if all_methods else df.loc[df.z_logvar_regularisation.isin(('L1', 'col_L1_dec'))]).groupby([
+            'z_dim',
+            'z_logvar_regularisation',
+        ])
 
-for use_orig_scale in [True, False]:
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(16, 8), sharey='row')
+        ncols = df.z_logvar_regularisation.nunique() if all_methods else 2
+        nrows = df.z_dim.nunique()
 
-    for (key, ax) in zip(grouped.groups.keys(), axes.flatten()):
-        subplot = grouped.get_group(key).plot.scatter(
-            x='lambda_logvar_regularisation',
-            y='test_rec_error',
-            logx=True,
-            label=key,
-            ax=ax,
-        )
-        ax.set_xlim((5e-5, 12.5))
-        if use_orig_scale:
-            ax.set_ylim((6350, 6550) if key[0] == 32 else (6250, 6450))
+        fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(16, 8), sharey='row')
 
-    plt.savefig(PLOTS_DIR / f'preliminary_{"orig" if use_orig_scale else "auto"}_scale.png')
-    plt.show()
+        for (key, ax) in zip(grouped.groups.keys(), axes.flatten()):
+            subplot = grouped.get_group(key).plot.scatter(
+                x='lambda_logvar_regularisation',
+                y='test_rec_error',
+                logx=True,
+                label=key,
+                ax=ax,
+            )
+            ax.legend(loc='upper left')
+            ax.set_xlim((5e-5, 15.5))
+            if use_orig_scale:
+                ax.set_ylim((6350, 6550) if key[0] == 32 else (6250, 6450))
+
+        plt.savefig(PLOTS_DIR / f'{"orig" if use_orig_scale else "auto"}_scale{"_all" if all_methods else ""}.png')
+        plt.show()
