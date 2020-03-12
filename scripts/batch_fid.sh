8dda45732e3281b62ea99d4e81855e331bb03ddc
alexander.rakowski <alexander.rakowski@hpi.de>
2020-03-12 16:37:27 +0100

    fix batch_fid script

--- scripts/batch_fid.sh
+++ scripts/batch_fid.sh
@@ -2,7 +2,7 @@ CHECKPOINT_PATHS=$(find $1 -type f -name 'checkpoint')
 for CHECKPOINT_PATH in $CHECKPOINT_PATHS; do
   EXPERIMENT_PATH=$(dirname $(dirname $CHECKPOINT_PATH))
   FID_PATH=${EXPERIMENT_PATH}/test_fid.txt
-  if [ ! -f ${FID_PATH} ]; then
+  if [ -f ${FID_PATH} ]; then
     echo "${FID_PATH} exists - skipping directory"
   else
     python fid.py --experiment_path $EXPERIMENT_PATH
