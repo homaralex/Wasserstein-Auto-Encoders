69967c720018d5e76ea986d5fc5d519737acb51d
alexander.rakowski <alexander.rakowski@hpi.de>
2020-03-17 19:37:56 +0100

    batch_active_dims.sh

--- scripts/batch_active_dims.sh
+++ scripts/batch_active_dims.sh
@@ -1,9 +1,9 @@
 CHECKPOINT_PATHS=$(find $1 -type f -name 'checkpoint')
 for CHECKPOINT_PATH in $CHECKPOINT_PATHS; do
   EXPERIMENT_PATH=$(dirname $(dirname $CHECKPOINT_PATH))
-  FID_PATH=${EXPERIMENT_PATH}/num_active_dims.txt
-  if [ -f ${FID_PATH} ]; then
-    echo "${FID_PATH} exists - skipping directory"
+  ACTIVE_DIMS_PATH=${EXPERIMENT_PATH}/num_active_dims.txt
+  if [ -f ${ACTIVE_DIMS_PATH} ]; then
+    echo "${ACTIVE_DIMS_PATH} exists - skipping directory"
   else
     python active_dims.py --experiment_path $EXPERIMENT_PATH
   fi
