20e97ec1324b4f1ae29d1cc801f98a90221aee2b
alexander.rakowski <alexander.rakowski@hpi.de>
2020-04-17 18:06:06 +0200

    metrics - add explicitness

--- disentanglement_metrics.py
+++ disentanglement_metrics.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 
 import gin
+import numpy as np
 import disentanglement_lib.evaluation.metrics.utils as dlib_metrics_utils
 import disentanglement_lib.evaluation.metrics.mig as dlib_mig
 import disentanglement_lib.evaluation.metrics.modularity_explicitness as dlib_modularity
@@ -20,7 +21,14 @@ class DisentanglementMetrics(Disentanglement):
         ys = self.sample_latent(size=num_points)
         xs = self.imgs[self.latent_to_index(ys)][:, :, :, None]
 
-        return xs, ys
+        zs = self.model.encode(xs)
+
+        # transpose as numpy expects num_factors as first dimension
+        # also, take only 5 last factors as the first one is a placeholder - which will result in 0 entropy, and thus
+        # division by zero in some metrics
+        zs, ys = zs.transpose(), ys.transpose()[1:]
+
+        return zs, ys
 
     def mig(self, zs, ys):
         config_path = METRIC_CONFIGS_PATHS['mig']
@@ -37,7 +45,27 @@ class DisentanglementMetrics(Disentanglement):
         discretized_mus = dlib_metrics_utils.make_discretizer(zs)
         mutual_information = dlib_metrics_utils.discrete_mutual_info(discretized_mus, ys)
 
-        scores = {'modularity_score': dlib_modularity.modularity(mutual_information)}
+        return {'modularity_score': dlib_modularity.modularity(mutual_information)}
+
+    def explicitness(self, zs, ys):
+        config_path = METRIC_CONFIGS_PATHS['modularity_explicitness']
+        gin.parse_config_file(config_file=str(config_path))
+
+        mus_train, ys_train = zs, ys
+        mus_test, ys_test = self._get_data_points(num_points=ys.shape[1] // 2)
+
+        scores = {}
+        explicitness_score_train = np.zeros([ys_train.shape[0], 1])
+        explicitness_score_test = np.zeros([ys_test.shape[0], 1])
+        mus_train_norm, mean_mus, stddev_mus = dlib_metrics_utils.normalize_data(mus_train)
+        mus_test_norm, _, _ = dlib_metrics_utils.normalize_data(mus_test, mean_mus, stddev_mus)
+        for i in range(ys_train.shape[0]):
+            explicitness_score_train[i], explicitness_score_test[i] = dlib_modularity.explicitness_per_factor(
+                mus_train_norm, ys_train[i, :],
+                mus_test_norm, ys_test[i, :],
+            )
+        scores["explicitness_score_train"] = np.mean(explicitness_score_train)
+        scores["explicitness_score_test"] = np.mean(explicitness_score_test)
 
         return scores
 
@@ -45,17 +73,12 @@ class DisentanglementMetrics(Disentanglement):
         # cast to int as scientific notation numbers are floats
         num_points = int(num_points)
 
-        xs, ys = self._get_data_points(num_points=num_points)
-        zs = self.model.encode(xs)
-
-        # transpose as numpy expects num_factors as first dimension
-        # also, take only 5 last factors as the first one is a placeholder - which will result in 0 entropy, and thus
-        # division by zero in some metrics
-        zs, ys = zs.transpose(), ys.transpose()[1:]
+        zs, ys = self._get_data_points(num_points=num_points)
 
         metrics = {}
         metrics.update(self.mig(zs=zs, ys=ys))
         metrics.update(self.modularity(zs=zs, ys=ys))
+        metrics.update(self.explicitness(zs=zs, ys=ys))
 
         return metrics
 
